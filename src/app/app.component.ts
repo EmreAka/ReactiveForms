81ff635c3c734eace271fe354252fcaf3be1a3c7
Emre <aka548548@gmail.com>
2022-08-12 04:05:29 +0300

    Reactive Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ReactiveForms';
+
+  testform: UntypedFormGroup;
+  
+  constructor(private ufb: UntypedFormBuilder) { }
+
+  get addresses(){
+    return this.testform.controls["addresses"] as FormArray;
+  }
+
+  ngOnInit(){
+    this.testform = this.ufb.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.min(5)]],
+      addresses: this.ufb.array([]) 
+    })
+
+    this.testform.valueChanges.subscribe({
+      next: (value) => {console.log(value)}
+    });
+  }
+
+  addAddress(){
+    const adressForm = this.ufb.group({
+      adress: ["", Validators.required]
+    })
+
+    this.addresses.push(adressForm);
+  }
+
 }
